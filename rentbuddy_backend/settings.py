d18f5f873ab514399955dbcdc3ad9ebd2745e14b
Raphael <naduma.raphael4@gmail.com>
2025-03-11 10:45:57 +0800

    Revert "user authentication APIs"
    This reverts commit 139aad0969e5a94a29e6aacf1540dbcb556db2fd.

--- rentbuddy_backend/settings.py
+++ rentbuddy_backend/settings.py
@@ -122,11 +122,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-INSTALLED_APPS += ['rest_framework', 'rest_framework_simplejwt']
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
--- rentbuddy_backend/urls.py
+++ rentbuddy_backend/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
-from django.urls import path, include
-
-# Simple homepage view
-def home(request):
-    return HttpResponse("Welcome to RentBuddy Backend!")
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),  # This adds a homepage
-    path('auth/', include('authentication.urls')),
 ]
