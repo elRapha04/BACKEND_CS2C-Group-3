9a7f5ed1a77aeff3412680f0e8591f55df16f05f
Raphael <naduma.raphael4@gmail.com>
2025-03-12 12:19:27 +0800

    revert

--- authentication/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import CustomUser 
-from .serializers import UserSerializer
-
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
